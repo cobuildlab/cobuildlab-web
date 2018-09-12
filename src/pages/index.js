a8e09d62cc4d148d791f92dacfe5c7f07ec8397b
Luis Vilchez <luisdv93@gmail.com>
2018-09-12 12:43:51 -0400

    upgrade to v2

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,33 +1,75 @@
-import React from 'react';
-import 'bootstrap';
-import Helmet from 'react-helmet';
-import Header from '../components/header';
-import Main from '../components/main';
+import React from 'react'
+import { Link, graphql } from 'gatsby'
+import get from 'lodash/get'
+import Helmet from 'react-helmet'
 
-const IndexPage = () => (
-  <div>
-    <Helmet>
-      <meta name="description"
-            content="Cobuild Lab is a technological entrepreneurship accelerator, focused on optimizing the transformation of ideas into profitable businesses"/>
-      <link rel="canonical" href="https://cobuild.com/"/>
-      <meta property="og:locale" content="en_US"/>
-      <meta property="og:type" content="website"/>
-      <meta property="og:title" content="Cobuild Lab | Idea Accelerator In Miami, Florida"/>
-      <meta property="og:description"
-            content="4Geeks Cobuild Lab is a technological entrepreneurship accelerator, focused on optimizing the transformation of ideas into profitable businesses"/>
-      <meta property="og:url" content="https://cobuildlab.com/"/>
-      <meta property="og:site_name" content="Cobuild Lab"/>
-      <meta name="twitter:card" content="summary"/>
-      <meta name="twitter:description"
-            content="4Geeks Cobuild Lab is a technological entrepreneurship accelerator, focused on optimizing the transformation of ideas into profitable businesses"/>
-      <meta name="twitter:title" content="Cobuild Lab | Idea Accelerator In Miami, Florida"/>
-      <meta name="twitter:site" content="@cobuildlab"/>
-      <meta name="twitter:creator" content="@cobuildlab"/>
-      <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Lato" />
-    </Helmet>
-    <Header/>
-    <Main/>
-  </div>
-);
+import Bio from '../components/Bio'
+import Layout from '../components/layout'
+import { rhythm } from '../utils/typography'
 
-export default IndexPage;
+class BlogIndex extends React.Component {
+  render() {
+    const siteTitle = get(this, 'props.data.site.siteMetadata.title')
+    const siteDescription = get(
+      this,
+      'props.data.site.siteMetadata.description'
+    )
+    const posts = get(this, 'props.data.allMarkdownRemark.edges')
+
+    return (
+      <Layout location={this.props.location}>
+        <Helmet
+          htmlAttributes={{ lang: 'en' }}
+          meta={[{ name: 'description', content: siteDescription }]}
+          title={siteTitle}
+        />
+        <Bio />
+        {posts.map(({ node }) => {
+          const title = get(node, 'frontmatter.title') || node.fields.slug
+          return (
+            <div key={node.fields.slug}>
+              <h3
+                style={{
+                  marginBottom: rhythm(1 / 4),
+                }}
+              >
+                <Link style={{ boxShadow: 'none' }} to={node.fields.slug}>
+                  {title}
+                </Link>
+              </h3>
+              <small>{node.frontmatter.date}</small>
+              <p dangerouslySetInnerHTML={{ __html: node.excerpt }} />
+            </div>
+          )
+        })}
+      </Layout>
+    )
+  }
+}
+
+export default BlogIndex
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+        description
+      }
+    }
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      edges {
+        node {
+          excerpt
+          fields {
+            slug
+          }
+          frontmatter {
+            date(formatString: "DD MMMM, YYYY")
+            title
+          }
+        }
+      }
+    }
+  }
+`
