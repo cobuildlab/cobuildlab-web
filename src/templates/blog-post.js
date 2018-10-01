bd5b46f2057b668497c0f5e5662e3b74fc8d9c3e
Jorge Pernia <jorpg23@gmail.com>
2018-10-01 15:21:53 -0400

    fix style post header responsive

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -86,7 +86,7 @@ class BlogPostTemplate extends React.Component {
           />
         </Hero>
 
-        <section className="section">
+        <section id="section-post" className="section">
           <Container>
             <Columns isCentered>
               <Column
