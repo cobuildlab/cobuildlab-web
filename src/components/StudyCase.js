705e0b65ffa124b936101cce322cb9b5c0dd0a2b
Luis Vilchez <luisdv93@gmail.com>
2018-12-21 10:48:51 -0400

    changes to study case

--- src/components/StudyCase.js
+++ src/components/StudyCase.js
@@ -7,23 +7,10 @@ const StudyCase = props => {
   return (
     <Media>
       <MediaLeft>
-        <img
-          //src={logo}
-          src="https://via.placeholder.com/160x160"
-          className="study-image"
-        />
+        <img src={logo} className="study-image" />
       </MediaLeft>
       <MediaContent>
-        <p className="study-text">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat
-          odit earum numquam voluptas modi incidunt exercitationem excepturi
-          animi, facilis vero recusandae magni reiciendis culpa neque cum vitae
-          ducimus similique minima!Voluptatibus unde repudiandae, libero totam
-          eos autem, quisquam facilis voluptates quae dolores architecto quo
-          aperiam error consectetur veritatis nesciunt reprehenderit distinctio,
-          ullam possimus doloribus corrupti sint molestiae placeat. Quibusdam,
-          natus.
-        </p>
+        <p className="study-text">{text}</p>
 
         <h2 className="study-subtitle">Outcomes</h2>
 
