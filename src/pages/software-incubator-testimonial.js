f670e4edb0b7893c8549e5b9ba39d03c0c76c40d
Luis Vilchez <luisdv93@gmail.com>
2018-12-24 18:45:15 -0400

    add new pages

---
+++ src/pages/software-incubator-testimonial.js
@@ -0,0 +1,74 @@
+import React from 'react'
+import Helmet from 'react-helmet'
+import 'react-toastify/dist/ReactToastify.min.css'
+import Testimonial from '../components/Testimonial'
+import NewLandingForm from '../components/NewLadingForm'
+import { Title } from 'bloomer'
+import 'bulma'
+import '../assets/fonts/Lato-Black.ttf'
+import '../assets/fonts/Lato-BlackItalic.ttf'
+import '../assets/fonts/Lato-Bold.ttf'
+import '../assets/fonts/Lato-BoldItalic.ttf'
+import '../assets/fonts/Lato-Hairline.ttf'
+import '../assets/fonts/Lato-HairlineItalic.ttf'
+import '../assets/fonts/Lato-Italic.ttf'
+import '../assets/fonts/Lato-Light.ttf'
+import '../assets/fonts/Lato-LightItalic.ttf'
+import '../assets/fonts/Lato-Regular.ttf'
+
+import logo from '../resources/cobuildlab.png'
+import badge1 from '../resources/badges1.png'
+import badge2 from '../resources/badges2.png'
+import badge3 from '../resources/badges3.png'
+import badge4 from '../resources/badges4.png'
+import weedLogo from '../resources/weedmatch.png'
+import clienteImg from '../resources/oscar-weedmatch.png'
+
+class Index extends React.Component {
+  render() {
+    const siteTitle = 'Cobuild: Software Incubator in Miami'
+    const siteDescription =
+      'We help entrepreneurs create amazing Products with our streamlined, lean and phased Software Development Process. Since day one, our technique is focused on early results and transparent communication.'
+    const landingName = 'Software Incubator Testimonial'
+    const text =
+      'Lorem ipsum dolor sit amet, maecenas dictum in est, tincidunt nec suscipit vitae et et est. Justo vel lorem taciti elementum. Suspendisse et integer, aliquam nostra sollicitudin lacus, molestiae sit praesent ultrices augue arcu elit. Risus eu nulla varius, eu est morbi, lacinia elit vivamus phasellus volutpat sed mauris, nascetur vitae illo proin pulvinar, hac praesent varius.'
+
+    return (
+      <React.Fragment>
+        <Helmet
+          htmlAttributes={{ lang: 'en' }}
+          meta={[{ name: 'description', content: siteDescription }]}
+          title={siteTitle}
+        />
+        <section className="hero is-fullheight">
+          <div className="hero-body no-padding-hero">
+            <div className="content">
+              <div className="columns is-desktop">
+                <div className="column is-8 h-100 bg-header-landing">
+                  <div>
+                    <img src={logo} className="logo" />
+                  </div>
+                  <Title className="title-squeezing">
+                    Developing new Software <br /> Products in Miami
+                  </Title>
+                  <Testimonial image={clienteImg} logo={weedLogo} text={text} />
+                </div>
+                <div className="column h-100 is-4 bg-form-landing Aligner">
+                  <div className="has-text-centered view-badge">
+                    <img src={badge1} className="badge" />
+                    <img src={badge2} className="badge" />
+                    <img src={badge3} className="badge" />
+                    <img src={badge4} className="badge-google" />
+                  </div>
+                  <NewLandingForm landingName={landingName} />
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </React.Fragment>
+    )
+  }
+}
+
+export default Index
