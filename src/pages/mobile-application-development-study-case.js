c6d7d2d541c8c3617bbf905f49a4ad402a1ce973
Angel Lacret <alacret@gmail.com>
2019-01-12 15:25:01 -0500

    mobile application development

---
+++ src/pages/mobile-application-development-study-case.js
@@ -0,0 +1,76 @@
+import React from 'react'
+import Helmet from 'react-helmet'
+import 'react-toastify/dist/ReactToastify.min.css'
+import StudyCase from '../components/StudyCase'
+import NewLandingForm from '../components/NewLandingForm'
+import BadgeIndustry from '../components/BadgeIndustry'
+import { Title } from 'bloomer'
+import 'bulma'
+import '../assets/fonts/Lato-Black.ttf'
+import '../assets/fonts/Lato-BlackItalic.ttf'
+import '../assets/fonts/Lato-Bold.ttf'
+import '../assets/fonts/Lato-BoldItalic.ttf'
+import '../assets/fonts/Lato-Hairline.ttf'
+import '../assets/fonts/Lato-HairlineItalic.ttf'
+import '../assets/fonts/Lato-Italic.ttf'
+import '../assets/fonts/Lato-Light.ttf'
+import '../assets/fonts/Lato-LightItalic.ttf'
+import '../assets/fonts/Lato-Regular.ttf'
+
+import logo from '../resources/cobuildlab.png'
+import badge1 from '../resources/badges1.png'
+import badge2 from '../resources/badges2.png'
+import badge3 from '../resources/badges3.png'
+import badge4 from '../resources/badges4.png'
+
+export default class AppsDevStudy extends React.Component {
+  render() {
+    const siteTitle = "Mobile Application Development in Miami";
+    const siteSubtitle = 'We transform ideas into working Mobile Application';
+    const siteDescription = "Let's create amazing Mobile Applications";
+    const siteLongDescription = 'We transform requirements into working Software Products. We specialize in building web and mobile applications for Small and Medium Businesses.'
+    const landingName = 'Mobile Application Development';
+
+    return (
+      <React.Fragment>
+        <Helmet
+          htmlAttributes={{ lang: 'en' }}
+          meta={[{ name: 'description', content: siteDescription }]}
+          title={siteTitle}
+        />
+        <section className="hero is-fullheight">
+          <div className="hero-body no-padding-hero">
+            <div className="content">
+              <div className="columns is-desktop">
+                <div className="column is-8 h-100 bg-header-study">
+                  <div className="view-content-zoom">
+                    <div>
+                      <img src={logo} className="logo" />
+                    </div>
+                    <Title className="title-squeezing">
+                    {siteTitle}
+                    </Title>
+                    <h2 className="subtitle-squeezing">{siteSubtitle}</h2>
+                    <p className="text-description-all">{siteLongDescription}</p>
+                    <StudyCase />
+                    <BadgeIndustry />
+                  </div>
+                </div>
+
+                <div className="column h-100 is-4 bg-form-landing Aligner">
+                  <div className="has-text-centered view-badge">
+                    <img src={badge1} className="badge" />
+                    <img src={badge2} className="badge" />
+                    <img src={badge3} className="badge" />
+                    <img src={badge4} className="badge-google" />
+                  </div>
+                  <NewLandingForm landingName={landingName} />
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </React.Fragment>
+    )
+  }
+}
