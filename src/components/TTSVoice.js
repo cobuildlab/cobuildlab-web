8552367d7aec4454964b68d47d9e6860b7644a10
Angel Lacret <alacret@gmail.com>
2019-10-27 12:50:20 -0400

    quick fix

--- src/components/TTSVoice.js
+++ src/components/TTSVoice.js
@@ -1,39 +1,42 @@
 import React from 'react'
 import { Button, Title } from 'bloomer'
 
-let speechSynthesis = null
-const speech = (text) => {
-  try {
-    speechSynthesis = window.speechSynthesis
-    const utterance = new SpeechSynthesisUtterance(text)
-    // Set utterance properties
-    utterance.voice = speechSynthesis.getVoices().filter(function(voice) {
-      return voice.lang === 'en'
-    })[0]
-    utterance.pitch = 1
-    utterance.rate = 0.8
-    utterance.volume = 0.8
-    speechSynthesis.speak(utterance)
-  } catch (e) {
-    console.log('SPEECH', e)
-  }
-}
-const pause = () => {
-  if (speechSynthesis)
-    speechSynthesis.pause()
-}
-
 const TTSVoice = (props) => {
   const [isPlaying, setIsPlaying] = React.useState(false)
   const stripped = props.text.replace(/<(?:.|\n)*?>/gm, '')
+  const speech = (text) => {
+    console.log('TTSVoice:SPEECH:', text)
+    try {
+      const speechSynthesis = window.speechSynthesis
+      speechSynthesis.cancel()
+      const utterance = new SpeechSynthesisUtterance(text)
+      // Set utterance properties
+      utterance.voice = speechSynthesis.getVoices().filter(function(voice) {
+        return voice.lang === 'en'
+      })[0]
+      utterance.pitch = 1
+      utterance.rate = 0.8
+      utterance.volume = 0.8
+      console.log('TTSVoice:SPEECH:', speechSynthesis, utterance)
+      speechSynthesis.speak(utterance)
+    } catch (e) {
+      console.log('SPEECH', e)
+    }
+  }
+  const cancel = () => {
+    console.log('TTSVoice:PAUSE:', speechSynthesis)
+    if (window.speechSynthesis)
+      window.speechSynthesis.cancel()
+  }
+
   return (
     <React.Fragment>
       <Title isSize={2}>Text to Speech:</Title>
       {isPlaying ?
         <Button isColor={'primary'} onClick={() => {
-          pause(stripped)
+          cancel(stripped)
           setIsPlaying(false)
-        }}>Pause</Button>
+        }}>Cancel</Button>
         :
         <Button isColor={'primary'} onClick={() => {
           speech(stripped)
