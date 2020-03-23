d7098a7d5d905baf7b5e5d1cf243aab1c606e036
carlos <kikeztw@gmail.com>
2020-03-23 11:35:23 -0400

    finish customer success stories

--- src/pages/2020/components/CustomerSuccessStories/index.js
+++ src/pages/2020/components/CustomerSuccessStories/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
 import { Container } from 'bloomer';
 import Slider from 'react-slick';
 import Typography from '@2020/components/Typography';
@@ -19,14 +20,13 @@ const title = {
 
 const settings = {
   className: 'customer-success-stories-carousel',
+  touchMove: false,
   centerMode: true,
-  // infinite: true,
-  // autoplay: true,
+  infinite: true,
+  autoplay: true,
   slidesToShow: 3,
-  slidesToScroll: 4,
   speed: 2500,
   variableWidth: true,
-  // adaptiveHeight: true,
   responsive: [
     {
       breakpoint: 480,
@@ -35,22 +35,39 @@ const settings = {
         slidesToShow: 1,
         adaptiveHeight: true,
         slidesToScroll: 0,
-        speed: 500,
-        // adaptiveHeight: true,
+        speed: 1000,
+        autoplay: false,
+        touchMove: true,
       },
     },
   ],
 };
 
-const Item = () => (
-  <div style={{ width: '100%', height: 385, display: 'flex', alignItems: 'center' }}>
-    <div
-      className="carousel-item"
-      style={{ width: '100%', height: '80%', backgroundColor: '#264A60', margin: 'auto' }}></div>
-  </div>
-);
-
 const CustomerSuccessStories = () => {
+  const data = useStaticQuery(pageQuery);
+
+  // dont remove inline styles or react slick is going to overwrite the css class
+
+  const items = data.customerSuccessStories.edges.map(({ node }) => (
+    <div style={{ width: 300 }} key={node.fields.slug}>
+      <div
+        style={{
+          width: '100%',
+          height: 385,
+          display: 'flex',
+          alignItems: 'center',
+        }}>
+        <div className="carousel-item">
+          <img
+            src={node?.frontmatter?.image?.publicURL}
+            alt={node?.frontmatter?.title}
+            style={{ width: '100%', height: '100%' }}
+          />
+        </div>
+      </div>
+    </div>
+  ));
+
   return (
     <Container isFluid className="container-not-margin">
       <div className="section-title">
@@ -58,31 +75,38 @@ const CustomerSuccessStories = () => {
           Customer Success Stories
         </Typography>
       </div>
-      <Slider {...settings}>
-        <div style={{ width: 300 }}>
-          <Item />
-        </div>
-        <div style={{ width: 300 }}>
-          <Item />
-        </div>
-        <div style={{ width: 300 }}>
-          <Item />
-        </div>
-        <div style={{ width: 300 }}>
-          <Item />
-        </div>
-        <div style={{ width: 300 }}>
-          <Item />
-        </div>
-        <div style={{ width: 300 }}>
-          <Item />
-        </div>
-        <div style={{ width: 300 }}>
-          <Item />
-        </div>
-      </Slider>
+      <Slider {...settings}>{items}</Slider>
     </Container>
   );
 };
+
+export const pageQuery = graphql`
+  query {
+    customerSuccessStories: allMarkdownRemark(sort: {fields: [frontmatter___date], order: DESC}, filter: {fileAbsolutePath: {regex: "/(pages/customer-success-stories)/.*\\.md$/"}}) {
+      edges {
+        node {
+          excerpt
+          fields {
+            slug
+          }
+          frontmatter {
+            title
+            tags
+            image {
+              publicURL
+              childImageSharp {
+                fluid(maxWidth: 480) {
+                  aspectRatio
+                  src
+                  srcSet
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
 
 export default CustomerSuccessStories;
