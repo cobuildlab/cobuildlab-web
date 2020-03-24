907c452c59d4e10d3fbbd83b071e0d010560fd3c
carlos <kikeztw@gmail.com>
2020-03-24 12:27:06 -0400

    finish footer

--- src/components/2020/Layout/index.js
+++ src/components/2020/Layout/index.js
@@ -1,23 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Footer from './Footer';
-//TODO add this file to index.scss and import this here
+import { Footer as FooterContainer } from 'bloomer';
+import Footer from '../Footer';
 import '../../../assets/css/2020/common.scss';
 import 'bulma';
 
-export default class Template extends React.Component {
-  render() {
-    const { children } = this.props;
-    const siteKey = process.env.RECAPTCHA_SITEKEY;
-    return (
-      <div style={{ overflow: 'hidden' }}>
-        {children}
-        <Footer siteKey={siteKey} />
-      </div>
-    );
-  }
-}
+const Layout = ({ children }) => (
+  <div style={{ overflow: 'hidden' }}>
+    {children}
+    <FooterContainer>
+      <Footer />
+    </FooterContainer>
+  </div>
+);
 
-Template.propTypes = {
+Layout.propTypes = {
   children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.node]).isRequired,
 };
+
+export default Layout;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,7 @@
 import React, { PureComponent } from 'react';
-import { graphql } from 'gatsby';
 import Helmet from 'react-helmet';
 import get from 'lodash/get';
 import { HeroHeader, HeroBody, Hero, Section } from 'bloomer';
-
-import 'react-toastify/dist/ReactToastify.min.css';
-import 'react-modal-video/scss/modal-video.scss';
-import 'bulma';
 
 import Layout from '../components/2020/Layout';
 import Header from '../components/2020/Layout/Header';
@@ -17,6 +12,10 @@ import CustomerSuccessStories from '../components/2020/CustomerSuccessStories';
 import OurTeam from '../components/2020/OurTeam';
 import Contact from '../components/2020/Contact';
 import Newsletter from '../components/2020/NewsLetter';
+
+import 'react-toastify/dist/ReactToastify.min.css';
+import 'react-modal-video/scss/modal-video.scss';
+import 'bulma';
 
 export default class HomePage extends PureComponent {
   constructor(props) {
@@ -36,11 +35,6 @@ export default class HomePage extends PureComponent {
   render() {
     const siteTitle = get(this, 'props.data.site.siteMetadata.title');
     const siteDescription = get(this, 'props.data.site.siteMetadata.description');
-    //TODO move this request to Customer Succes Stories
-    // const posts = get(this, 'props.data.allMarkdownRemark.edges');
-    // const customerSuccessStories = get(this, 'props.data.customerSuccessStories.edges');
-    // const siteKey = process.env.RECAPTCHA_SITEKEY;
-
     return (
       <Layout>
         <Helmet
@@ -78,45 +72,3 @@ export default class HomePage extends PureComponent {
     );
   }
 }
-
-export const pageQuery = graphql`
-  query {
-    site {
-      siteMetadata {
-        title
-        description
-      }
-    }
-    allMarkdownRemark(
-      sort: { fields: [frontmatter___date], order: DESC }
-      limit: 3
-      filter: {fileAbsolutePath: {regex: "/(pages/blog)/.*\\.md$/"}}
-    ) {
-      edges {
-        node {
-          excerpt
-          fields {
-            slug
-          }
-          frontmatter {
-            date(formatString: "MMMM DD, YYYY")
-            title
-            tags
-            image {
-              publicURL
-              childImageSharp {
-                fluid(maxWidth: 480) {
-                  aspectRatio
-                  base64
-                  sizes
-                  src
-                  srcSet
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-`;
