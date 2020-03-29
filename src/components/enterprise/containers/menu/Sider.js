f06b607f8a535cdbd38081f86d3d9f8d3e1082e4
carlos <kikeztw@gmail.com>
2020-03-29 11:57:44 -0400

    fixed: add suport to sider for ssr

--- src/components/enterprise/containers/menu/Sider.js
+++ src/components/enterprise/containers/menu/Sider.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import ReactDOM from 'react-dom';
 import { Icon } from 'react-icons-kit';
@@ -6,32 +6,59 @@ import { thickLeft } from 'react-icons-kit/iconic/thickLeft';
 import styles from './css/index.module.scss';
 import componse from '../../../../utils/styles-componse';
 
-const Sider = ({ visible, onClose }) => {
-  if (!document) return null;
 
-  const overlayStyle = componse(styles.overlay, visible ? styles.show : styles.hidden);
+const container = typeof document !== 'undefined' ? document.getElementsByTagName("body")[0] : null;
 
-  const menuStyles = componse(styles.menu, visible ? styles.show : styles.hidden);
 
-  const headerStyle = componse(styles.menu_item);
+export default class Sider extends PureComponent {
 
-  return ReactDOM.createPortal(
-    <div className={styles.sider}>
-      <div className={overlayStyle} onClick={onClose} />
-      <ul className={menuStyles}>
-        <li className={headerStyle} onClick={onClose}>
-          <Icon size={24} icon={thickLeft} />
-        </li>
-        <li className={styles.menu_item}>hola</li>
-        <li className={styles.menu_item}>hola</li>
-        <li className={styles.menu_item}>hola</li>
-        <li className={styles.menu_item}>hola</li>
-        <li className={styles.menu_item}>hola</li>
-      </ul>
-    </div>,
-    document.body,
-  );
-};
+  constructor() {
+    super();
+    // Use a ternary operator to make sure that the document object is defined
+    this.el = typeof document !== 'undefined' ? document.createElement('div') : null;
+  }
+
+  componentDidMount = () => {    
+    container.appendChild(this.el);
+  }
+
+  componentWillUnmount = () => {
+    container.removeChild(this.el);
+  }
+
+  render() {
+    const { visible, onClose } = this.props;
+
+    const overlayStyle = componse(styles.overlay, visible ? styles.show : styles.hidden);
+
+    const menuStyles = componse(styles.menu, visible ? styles.show : styles.hidden);
+  
+    const headerStyle = componse(styles.menu_item);
+
+    if (this.el) {
+      return ReactDOM.createPortal(
+        <div className={styles.sider}>
+          <div className={overlayStyle} onClick={onClose} />
+          <ul className={menuStyles}>
+            <li className={headerStyle} onClick={onClose}>
+              <Icon size={24} icon={thickLeft} />
+            </li>
+            <li className={styles.menu_item}>hola</li>
+            <li className={styles.menu_item}>hola</li>
+            <li className={styles.menu_item}>hola</li>
+            <li className={styles.menu_item}>hola</li>
+            <li className={styles.menu_item}>hola</li>
+          </ul>
+        </div>,
+        this.el,
+      );
+    } else {
+      return null;
+    }
+
+  }
+}
+
 
 Sider.defaultProps = {
   onClose: () => null,
@@ -43,4 +70,3 @@ Sider.propTypes = {
   visible: PropTypes.bool,
 };
 
-export default Sider;
