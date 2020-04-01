18acc1dbd647c1af095bb75ac33b88be70221086
carlos <kikeztw@gmail.com>
2020-04-01 12:57:07 -0400

    add custom button primary

--- src/components/2020/Button/index.js
+++ src/components/2020/Button/index.js
@@ -1,30 +1,60 @@
-import React, { memo } from 'react';
+import React, { memo, Fragment } from 'react';
 import PropTypes from 'prop-types';
-import { Button as BloomerButton } from 'bloomer';
 import { Link } from 'gatsby';
+import { Icon } from 'react-icons-kit';
+import { loadC } from 'react-icons-kit/ionicons/loadC';
+import Typography from '../Typography';
 import componse from '../../../utils/styles-componse';
 import styles from './css/index.module.scss';
 
-const Button = ({ children, to, onClick, className, ...rest }) => {
-  if (to) {
+const text = {
+  default: {
+    level: 11,
+    fontWeight: 'normal',
+  },
+};
+
+const Loading = ({ isLoading, children }) => {
+  const childrenContainerStyle = styles[`display_${isLoading ? 'hidden' : 'block'}`];
+  const iconContainerStyles = componse(
+    styles.icon_container,
+    !isLoading ? styles.display_hidden : '',
+  );
+
+  return (
+    <Fragment>
+      <span className={iconContainerStyles}>
+        <Icon size={20} icon={loadC} />
+      </span>
+      <div className={childrenContainerStyle}>
+        <Typography tag="span" size={text} className={styles.button_text}>
+          {children}
+        </Typography>
+      </div>
+    </Fragment>
+  );
+};
+
+Loading.propTypes = {
+  isLoading: PropTypes.bool.isRequired,
+  children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.node]).isRequired,
+};
+
+const Button = ({ children, to, onClick, className, isLoading, type, ...rest }) => {
+  const buttonStyles = componse(styles.button, styles[`button_${type}`], className);
+
+  if (to && to.length) {
     return (
-      <BloomerButton
-        {...rest}
-        render={(props) => (
-          <Link {...props} to={to} className={componse(styles.button_primary, props.className)} />
-        )}>
-        {children}
-      </BloomerButton>
+      <Link {...rest} to={to} className={buttonStyles}>
+        <Loading isLoading={isLoading}>{children}</Loading>
+      </Link>
     );
   }
 
   return (
-    <BloomerButton
-      {...rest}
-      onClick={onClick}
-      className={className ? componse(styles.button_primary, className) : styles.button_primary}>
-      {children}
-    </BloomerButton>
+    <button {...rest} className={buttonStyles}>
+      <Loading isLoading={isLoading}>{children}</Loading>
+    </button>
   );
 };
 
@@ -32,9 +62,13 @@ Button.defaultProps = {
   to: '',
   onClick: () => null,
   className: '',
+  isLoading: false,
+  type: 'primary',
 };
 
 Button.propTypes = {
+  type: PropTypes.string,
+  isLoading: PropTypes.bool,
   to: PropTypes.string,
   onClick: PropTypes.func,
   className: PropTypes.string,
