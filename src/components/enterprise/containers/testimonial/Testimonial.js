cd533e112e485d389be430074bec5e6acb57c810
carlos <kikeztw@gmail.com>
2020-04-08 16:47:44 -0400

    add testimonial item

--- src/components/enterprise/containers/testimonial/Testimonial.js
+++ src/components/enterprise/containers/testimonial/Testimonial.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import { Container, Section } from 'bloomer';
-import CarouselSlider from './Carousel';
+import Slider from 'react-slick';
 import Typography from '../../../2020/Typography';
-import './testimonial.scss';
 import styled from 'styled-components';
 import H2 from '../../../Typography/H2';
+import TestimonialItem from './TestimonialItem';
+import img1 from '../../../../assets/images/testimonial/andres-wegacha.jpeg';
+import img2 from '../../../../assets/images/testimonial/ale.jpg';
+import './testimonial.scss';
+
+const DATA = [
+  {
+    id: 1,
+    title: 'Andres Aguerrevere',
+    subtitle: 'Internal Dashboard Dev for Digital Marketing Agency',
+    img: img1,
+    content:
+      'Cobuild Lab Inc performed admirably in every aspect of the engagement, delivering a high-quality final product. Communicative and dedicated, the team developed a good understanding of the project and exceeded expectations in terms of quality and work ethic.',
+  },
+  {
+    id: 2,
+    title: 'Alejandro Sanchez',
+    subtitle: 'Jobs Marketplace Mobile App Dev for Hospitality Platform',
+    img: img2,
+    content:
+      'Cobuild Lab Inc delivered the product and continues to provide support to facilitate the release of new versions. The team identifies platform challenges and provides solutions to build a high-quality final product. They offer cost-effective services, which leads to continued collaboration.',
+  },
+];
 
 const Title = styled(H2)`
   font-size: calc(14px + (18 - 14) * ((100vw - 320px) / (1920 - 320)));
@@ -18,6 +40,15 @@ const Title = styled(H2)`
   font-weight: 400;
 `;
 
+const settings = {
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+};
+
 const Testimonial = () => {
   return (
     <Section isPaddingless id={'testimonials'}>
@@ -30,7 +61,21 @@ const Testimonial = () => {
                 Clients<span className="enterprise-text-orange sub-title">Feedback</span>
               </Typography>
             </div>
-            <CarouselSlider />
+            <div className="testimonial-slider">
+              <div className="testimonial-outer-box">
+                <Slider {...settings}>
+                  {DATA.map((data, index) => (
+                    <TestimonialItem
+                      key={data.title}
+                      title={data.title}
+                      subtitle={data.subtitle}
+                      description={data.content}
+                      img={data.img}
+                    />
+                  ))}
+                </Slider>
+              </div>
+            </div>
           </Container>
         </div>
       </div>
