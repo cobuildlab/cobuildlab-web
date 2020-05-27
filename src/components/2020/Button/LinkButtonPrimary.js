de238f47bb15f4811651c14ec4e6ef3a336c8555
carlos <kikeztw@gmail.com>
2020-05-27 18:22:06 -0400

    fix button

--- src/components/2020/Button/LinkButtonPrimary.js
+++ src/components/2020/Button/LinkButtonPrimary.js
@@ -13,11 +13,8 @@ const Redirect = styled(Link)`
 /**
  * @author github @kikeztw
  * @param {string} to -  Link prop for button, this property adds <a> tag at the same time as a button.
- * @param {string} className -  Styles for the button.
- * @param {boolean}   isLoading -   Loading indicator for button.
- * @param {string} type - Button type  default | primary | secondary.
- * @param {string} htmlType - Set the original html type of button see:
- *                         https://developer.mozilla.org/en-US/docs/Web/HTML/Element/button#attr-type.
+ * @param {boolean} isLoading -   Loading indicator for button.
+ * @param {string} type - Https://developer.mozilla.org/en-US/docs/Web/HTML/Element/button#attr-type.
  */
 
 const LinkButtonPrimary = ({ to, children, isLoading, isBlock, ...rest }) => (
