9b1e1748abd627797aaa7d8380cd6d9bbaa5ff00
Angel Lacret <alacret@gmail.com>
2020-05-28 10:09:23 -0400

    texto del startup

--- src/components/enterprise/containers/incubator/IncubatorContent.js
+++ src/components/enterprise/containers/incubator/IncubatorContent.js
@@ -33,12 +33,7 @@ const IncubatorContent = () => (
     <Title>
       StartUp your <TextOrange>idea</TextOrange>
     </Title>
-    <Text>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-      labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-      laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-      voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-    </Text>
+    <Text>Dedicate a team to think, design and build your Startup Idea.</Text>
   </Container>
 );
 
