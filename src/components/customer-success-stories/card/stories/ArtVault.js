1d33817d23ebf8da8e20074ffa2c4df57b24fb5d
eespinetti <eespinetti_99@hotmail.com>
2021-04-27 18:30:24 -0300

    CSS ARt Vault

---
+++ src/components/customer-success-stories/card/stories/ArtVault.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import Card from '../Card';
+import { StaticImage } from 'gatsby-plugin-image';
+import { Column } from 'bloomer';
+
+const ArtVault = () => {
+  return (
+    <Column isSize={{ mobile: 12, tablet: 4 }} isPaddingless>
+      <Card
+        title={'Art Vault'}
+        description={
+          <>
+            <p>
+              is an Art Management System that helps artists keep track of their inventory and the
+              purchasing of their pieces.
+            </p>
+            <span>
+              <b>Mobile</b>
+            </span>
+          </>
+        }
+        to={'/customer-success-stories/art-vault'}>
+        <StaticImage src={'./../../../../assets/images/customers/art-vault-logo.jpg'} alt="" />
+      </Card>
+    </Column>
+  );
+};
+
+export default ArtVault;
