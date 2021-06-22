5b4922bc8ed4732b32b471233dbf0cfb69354584
galue99 <edespinetti@gmail.com>
2021-06-22 10:41:35 -0300

    no index page in dev

--- src/html.js
+++ src/html.js
@@ -13,7 +13,7 @@ export default function HTML(props) {
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 
-        {process.env.BRANCH === 'dev' ? <meta name="robots" content="noindex" /> : ''}
+        {process.env.BRANCH === 'main' ? <meta name="robots" content="noindex" /> : ''}
         {/*}<!-- Replacement viewport meta tag (for AMP validity) -->*/}
         <meta
           name="viewport"
