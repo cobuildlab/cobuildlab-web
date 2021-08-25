3cfbde9c6b3946ca39d6588ce4aa52ad71c06966
Angel Lacret <alacret@gmail.com>
2021-08-25 12:54:48 -0400

    Update src/pages/referrals.js

--- src/pages/referrals.js
+++ src/pages/referrals.js
@@ -11,7 +11,7 @@ import ReferralsProgram from '../components/referrals/ReferralsProgram';
 
 const Referrals = () => (
   <Layout>
-    <SeoMetaTags title="Referrals" description="Privacy Policy for the Cobuild Lab" />
+    <SeoMetaTags title="Referrals" description="Referrals programs at Cobuild Lab" />
     <Hero>
       <HeroHeader>
         <Header />
