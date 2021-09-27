4c4dcdaefda2dd3aa1258480ea53f7212907ab4f
galue99 <edespinetti@gmail.com>
2021-09-27 14:20:54 -0300

    hide date field in articles card blog

--- src/components/blog-post/BlogPostCard.js
+++ src/components/blog-post/BlogPostCard.js
@@ -4,7 +4,7 @@ import BlogPostCardContainer from './BlogPostCardContainer';
 import BlogPostCardHeader from './BlogPostCardHeader';
 import BlogPostCardTitle from './BlogPostCardTitle';
 
-const PostCard = ({ src, title, to, date, category }) => (
+const PostCard = ({ src, title, to, category }) => (
   <BlogPostCardContainer alt={title} src={src} to={to}>
     <BlogPostCardHeader category={category} />
     <BlogPostCardTitle>{title}</BlogPostCardTitle>
@@ -15,7 +15,6 @@ PostCard.propTypes = {
   to: PropTypes.string.isRequired,
   src: PropTypes.object.isRequired,
   title: PropTypes.string.isRequired,
-  date: PropTypes.string.isRequired,
   category: PropTypes.string.isRequired,
 };
 
