e6ee2a6ed7dbe420a69508b2227da400e9049956
perdomofranklindev <perdomofranklindev@gmail.com>
2022-03-09 19:40:25 -0400

    some feature CSS fisherman

--- src/pages/customer-success-stories/fisherman.js
+++ src/pages/customer-success-stories/fisherman.js
@@ -1,252 +1,208 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import styled from 'styled-components';
 import DetailLayout from '../../components/customer-success-stories/DetailLayout';
-import BannerBackground from '../../components/2020/BannerBackground';
-import DetailHeroImagesContent from '../../components/customer-success-stories/DetailHeroImagesContent';
-import DetailTitle from '../../components/customer-success-stories/DetailTitle';
+import OverviewSection from '../../components/customer-success-stories/OverviewSection';
 import DetailParagraph from '../../components/customer-success-stories/DetailParagraph';
-import DetailSubTitle from '../../components/customer-success-stories/DetailSubTitle';
-import StickySection from '../../components/customer-success-stories/StickySection';
-import DetailSection from '../../components/customer-success-stories/new/DetailSection';
+import ProblemSection from '../../components/customer-success-stories/ProblemSection';
+import SolutionSection from '../../components/customer-success-stories/SolutionSection';
+import WhatWeDidSection from '../../components/customer-success-stories/WhatWeDidSection';
+import LogicalArchitectureSection from '../../components/customer-success-stories/LogicalArchitectureSection';
+import PhysicalArchitectureSection from '../../components/customer-success-stories/PhysicalArchitectureSection';
+import RoadmapSection from '../../components/customer-success-stories/RoadmapSection';
+import ResultSection from '../../components/customer-success-stories/ResultSection';
+import SlidersFisherman from '../../components/customer-success-stories/new/sliders/fisherman/Sliders';
+import OurGoalSection from '../../components/customer-success-stories/OurGoalSeaction';
+import OurTeamSection from '../../components/customer-success-stories/OurTeamSection';
 import { graphql } from 'gatsby';
 import { SeoMetaTags } from '../../components/2021/SeoMetaTags';
-import { Column, Columns, Container, Section } from 'bloomer';
 import { StaticImage } from 'gatsby-plugin-image';
-import { TextOrange } from '../../components/2021/text/TextHelpers';
 import { EpilogueSection } from '../../components/customer-success-stories/EpilogueSection';
+import { List, ListItem } from '../../components/Typography/List';
+import { TextIndigo } from '../../components/2021/text/TextHelpers';
+import { Section } from 'bloomer';
 
-const OverviewSection = styled.div`
-  margin-bottom: 5rem;
-`;
-
-const BannerOverview = styled(BannerBackground)`
-    top: '-40%',
-    right: '-80%',
-    width: '130%',
-    height: '130%',
-`;
-
-const BrandLogosContainer = styled.div`
-  display: flex;
-  align-items: center;
-  margin-top: 3.8rem;
-  @media screen and (max-width: 1024px) {
-    justify-content: center;
-  }
-`;
-
-const LightSection = styled.div`
-  background-color: #ffffff;
-  box-shadow: 0px 13px 15px 5px #00000024;
-`;
-
-const SolutionSection = styled.div`
-  display: block;
-`;
-
-const Fisherman = ({ data }) => (
-  <DetailLayout>
-    <SeoMetaTags
-      title="Fisherman"
-      description="Fisherman is an application interconnected with Slack and Github that helps development teams to perform their duties in a faster and more efficient way, such as standups, metrics, emails, tasks, and more."
-      image={data.seoImages.childImageSharp.resize}
-    />
-
+const Fisherman = ({ data }) => {
+  const team = [
     {
-      // Overview
-    }
-    <OverviewSection>
-      <BannerOverview />
-      <Section>
-        <Container>
-          <Columns isMultiline>
-            <Column isSize={{ mobile: 12, desktop: 6 }}>
-              <DetailTitle>Fisherman</DetailTitle>
-              <DetailParagraph isBold>
-                Fisherman is an application interconnected with Slack and Github that helps
-                development teams to perform their duties in a faster and more efficient way, such
-                as standups, metrics, emails, tasks, and more.
-              </DetailParagraph>
-              <BrandLogosContainer>
-                <StaticImage
-                  width={'67'}
-                  height={'68'}
-                  style={{ marginRight: '4rem' }}
-                  objectFit={'contain'}
-                  src={'../../assets/images/logos/brands-stories/google.png'}
-                  alt={'Google'}
-                />
-                <StaticImage
-                  width={'90'}
-                  height={'80'}
-                  style={{ marginRight: '4rem' }}
-                  objectFit={'contain'}
-                  src={'../../assets/images/logos/brands-stories/react.png'}
-                  alt={'React'}
-                />
-                <StaticImage
-                  width={'91'}
-                  height={'91'}
-                  objectFit={'contain'}
-                  src={'../../assets/images/logos/brands-stories/aws.png'}
-                  alt={'AWS'}
-                />
-              </BrandLogosContainer>
-            </Column>
-            <Column
-              style={{ display: 'flex', justifyContent: 'center' }}
-              isSize={{ mobile: 12, desktop: 6 }}>
-              <div style={{ width: '600px' }}>
-                <DetailHeroImagesContent>
-                  <StaticImage
-                    width={'1580'}
-                    height={'1084'}
-                    src={'./../../assets/images/customers/fisherman/overview-collage.png'}
-                    alt={'Overview collage'}
-                  />
-                </DetailHeroImagesContent>
-              </div>
-            </Column>
-          </Columns>
-        </Container>
-      </Section>
-    </OverviewSection>
-
+      first: 'Angel',
+      last: 'Lacret',
+      description: 'Chief of Product Development',
+      picture: (
+        <StaticImage
+          className={'team-member-img'}
+          src={'./../../assets/images/team/team-pictures/angel.jpg'}
+          alt={'Angel Lacret'}
+        />
+      ),
+    },
     {
-      // Problem
-    }
-    <LightSection>
-      <Section>
-        <Container>
-          <Columns isMultiline>
-            <Column isSize={{ tablet: 12, desktop: 6 }}>
-              <StickySection>
-                <StaticImage
-                  width={'1920'}
-                  height={'901'}
-                  src={'../../assets/images/customers/fisherman/problem.jpg'}
-                  alt={'problem'}
-                />
-              </StickySection>
-            </Column>
-            <Column isSize={{ tablet: 12, desktop: 6 }}>
-              <DetailSubTitle>
-                The <TextOrange> Problem </TextOrange>
-              </DetailSubTitle>
-              <DetailParagraph>
-                The opportunity stems from the need to help streamline processes and tasks related
-                to the daily work of software development agencies.
-              </DetailParagraph>
-              <DetailParagraph>
-                In addition, currently, there is no tool that allows monitoring the activities done
-                by a developer in GitHub and to match those metrics with stand up activity.
-              </DetailParagraph>
-            </Column>
-          </Columns>
-        </Container>
-      </Section>
-    </LightSection>
-
+      first: 'Alan',
+      last: 'R. Guevara',
+      description: 'Chief of Product Management',
+      picture: (
+        <StaticImage
+          className={'team-member-img'}
+          src={'../../assets/images/team/team-pictures/alanthinks.jpg'}
+          alt={'Alan R. Guevara'}
+        />
+      ),
+    },
     {
-      // Solution
-    }
-    <SolutionSection>
-      <DetailSection
+      first: 'Mayela',
+      last: 'Marin',
+      description: 'Project Manager',
+      picture: (
+        <StaticImage
+          className={'team-member-img'}
+          src={'./../../assets/images/team/new-pictures/green/mayela-marin.jpg'}
+          alt={'Mayela Marin'}
+        />
+      ),
+    },
+    {
+      first: 'Alexander',
+      last: 'Romero',
+      description: 'Full Stack Javascript Developer',
+      picture: (
+        <StaticImage
+          className={'team-member-img'}
+          src={'./../../assets/images/team/new-pictures/white/Alexander-Romero.jpg'}
+          alt={'Alexander Romero'}
+        />
+      ),
+    },
+    {
+      first: 'Xavier',
+      last: 'Romero',
+      description: 'Full Stack Javascript Developer',
+      picture: (
+        <StaticImage
+          className={'team-member-img'}
+          src={'./../../assets/images/team/new-pictures/green/Xavier-Romero-Developer-v.jpg'}
+          alt={'Xavier Romero'}
+        />
+      ),
+    },
+  ];
+
+  const goals = [
+    { goal: 100, subTitle: '100% increase of tracking productivity metrics' },
+    { goal: 30, subTitle: '30% reduce manual tasks to track issues' },
+    { goal: 30, subTitle: '30% reduce manual standups' },
+  ];
+
+  return (
+    <DetailLayout>
+      <SeoMetaTags
+        title="Fisherman"
+        description="Fisherman is an application interconnected with Slack and Github that helps development teams to perform their duties in a faster and more efficient way, such as standups, metrics, emails, tasks, and more."
+        image={data.seoImages.childImageSharp.resize}
+      />
+      <OverviewSection
+        title="Fisherman"
         image={
           <StaticImage
-            width={'1899'}
-            height={'900'}
-            src={'./../../assets/images/customers/fisherman/solution.jpg'}
-            alt={'Solution'}
+            width={'1580'}
+            height={'1084'}
+            src={'./../../assets/images/customers/fisherman/overview-collage.png'}
+            alt={'Overview collage'}
           />
-        }
-        caption={
-          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
-        }
-        left>
-        <DetailSubTitle>
-          The <TextOrange>Solution</TextOrange>
-        </DetailSubTitle>
+        }>
+        <DetailParagraph isBold>
+          Fisherman is an application interconnected with Slack and Github that helps development
+          teams to perform their duties in a faster and more efficient way, such as standups,
+          metrics, emails, tasks, and more.
+        </DetailParagraph>
+      </OverviewSection>
+      <ProblemSection>
+        <DetailParagraph>
+          The opportunity stems from the need to help streamline processes and tasks related to the
+          daily work of software development agencies.
+        </DetailParagraph>
+        <DetailParagraph>
+          In addition, currently, there is no tool that allows monitoring the activities done by a
+          developer in GitHub and to match those metrics with stand up activity.
+        </DetailParagraph>
+      </ProblemSection>
 
-        <DetailParagraph>
-          We used a low code platform to rapidly prototype and develop features as we moved into
-          discovering challenges for the development teams.
-        </DetailParagraph>
-        <DetailParagraph>
-          Users can set up email accounts for the projects, standup bots, and view metrics on how
-          developers are working on a project.
-        </DetailParagraph>
-        <DetailParagraph>
-          In this way, a continuous follow-up of activities is maintained, having available metrics
-          to evaluate the work performance in terms of designated kpi.
-        </DetailParagraph>
-        <Container>
-          <Columns>
-            <Column
-              style={{ display: 'flex', alignItems: 'center', marginTop: '2.5rem' }}
-              isSize={{ desktop: 12 }}>
-              <StaticImage
-                width={'67'}
-                height={'68'}
-                style={{ marginRight: '4rem' }}
-                objectFit={'contain'}
-                src={'../../assets/images/logos/brands-stories/google.png'}
-                alt={'Google'}
-              />
-              <StaticImage
-                width={'90'}
-                height={'80'}
-                style={{ marginRight: '4rem' }}
-                objectFit={'contain'}
-                src={'../../assets/images/logos/brands-stories/react.png'}
-                alt={'React'}
-              />
-              <StaticImage
-                width={'91'}
-                height={'91'}
-                style={{ marginRight: '4rem' }}
-                objectFit={'contain'}
-                src={'../../assets/images/logos/brands-stories/aws.png'}
-                alt={'AWS'}
-              />
-              <StaticImage
-                width={'91'}
-                height={'91'}
-                style={{ marginRight: '4rem' }}
-                objectFit={'contain'}
-                src={'../../assets/images/logos/brands-stories/aws.png'}
-                alt={'AWS'}
-              />
-              <StaticImage
-                width={'67'}
-                height={'68'}
-                objectFit={'contain'}
-                src={'../../assets/images/logos/brands-stories/google.png'}
-                alt={'Google'}
-              />
-            </Column>
-          </Columns>
-        </Container>
-      </DetailSection>
-    </SolutionSection>
+      <SolutionSection>
+        <>
+          <DetailParagraph>
+            We used a low code platform to rapidly prototype and develop features as we moved into
+            discovering challenges for the development teams.
+          </DetailParagraph>
+          <DetailParagraph>
+            Users can set up email accounts for the projects, standup bots, and view metrics on how
+            developers are working on a project.
+          </DetailParagraph>
+          <DetailParagraph>
+            In this way, a continuous follow-up of activities is maintained, having available
+            metrics to evaluate the work performance in terms of designated kpi.
+          </DetailParagraph>
+        </>
+      </SolutionSection>
 
-    <LightSection>
-      <Section>
-        <div style={{ textAlign: 'center' }}>
-          <DetailSubTitle>
-            What <TextOrange> We did </TextOrange>
-          </DetailSubTitle>
-        </div>
+      <WhatWeDidSection
+        logicalArchitecture={
+          <LogicalArchitectureSection
+            image={
+              <StaticImage
+                width={'960'}
+                height={'540'}
+                src={'./../../assets/images/customers/fisherman/logical-architecture.jpg'}
+                alt={'Logical Architecture'}
+              />
+            }>
+            <DetailParagraph>Lorem ipsum dolor.</DetailParagraph>
+          </LogicalArchitectureSection>
+        }
+        physicalArchitecture={
+          <PhysicalArchitectureSection>
+            <DetailParagraph>Lorem ipsum dolor.</DetailParagraph>
+          </PhysicalArchitectureSection>
+        }
+        roadmap={
+          <RoadmapSection>
+            <StaticImage
+              style={{ width: '100%', maxWidth: '1000px' }}
+              width={'960'}
+              height={'540'}
+              objectFit={'contain'}
+              src={'./../../assets/images/customers/fisherman/roadmap.jpg'}
+              alt={'Roadmap'}
+            />
+          </RoadmapSection>
+        }
+      />
+      <ResultSection>
+        <DetailParagraph>
+          A web application for company managers to create projects on the platform, and be able to
+          associate these projects with the Github repository and Slack workspaces to track,
+          measure, and increase developer’s teams productivity.
+        </DetailParagraph>
+        <List>
+          <ListItem>
+            <TextIndigo style={{ fontWeight: 'bold' }}>Location: </TextIndigo>
+            {'  '}Miami, FL
+          </ListItem>
+          <ListItem>
+            <TextIndigo style={{ fontWeight: 'bold' }}>Industry: </TextIndigo> Software development
+          </ListItem>
+          <ListItem>
+            <TextIndigo style={{ fontWeight: 'bold' }}>Product: </TextIndigo> Platform to track,
+            measure, and increase developer’s teams productivity
+          </ListItem>
+        </List>
+      </ResultSection>
+      <OurGoalSection data={goals} />
+      <Section isPaddingless style={{ marginBottom: '6rem' }}>
+        <SlidersFisherman />
       </Section>
-      <DetailSection>
-        <DetailSubTitle isBold={false}>Logical Architecture</DetailSubTitle>
-      </DetailSection>
-    </LightSection>
-
-    <EpilogueSection />
-  </DetailLayout>
-);
+      <OurTeamSection data={team} />
+      <EpilogueSection />
+    </DetailLayout>
+  );
+};
 
 Fisherman.propTypes = {
   data: PropTypes.object.isRequired,
@@ -254,7 +210,7 @@ Fisherman.propTypes = {
 
 export const pageQuery = graphql`
   query {
-    seoImages: file(relativePath: { eq: "customers/art-play/art-play-logo-black.png" }) {
+    seoImages: file(relativePath: { eq: "customers/fisherman/fisherman-logo.png" }) {
       childImageSharp {
         resize(width: 1200, height: 1200) {
           width
