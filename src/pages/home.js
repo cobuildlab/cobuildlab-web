df767e1a91a822b5f4cd4e810246495b6f0c6a19
perdomofranklindev <perdomofranklindev@gmail.com>
2022-05-11 13:07:47 -0400

    Home page componentes have been refactored

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import { SeoMetaTags } from '../components/2021/SeoMetaTags';
+import { Container } from 'bloomer';
 import 'bulma/css/bulma.css';
-import Theme from '../components/2022/Theme';
-import Layout from '../components/2022/Layout';
+import Theme from '../components/2022/theme/Theme';
 import Header from '../components/2022/headers/Header';
+import HomeBanner from '../components/2022/home/HomeBanner';
+import Introduction from '../components/2022/home/Introduction';
+import HomeHero from '../components/2022/home/HomeHero';
+import Footer from '../components/2022/footers/Footer';
 
-const Home = () => {
-  console.log('Home page rendered');
-  return (
-    <Theme>
-      <Layout>
-        <SeoMetaTags lang="en" />
-        <Header />
-      </Layout>
-    </Theme>
-  );
-};
+const Home = () => (
+  <Theme>
+    <SeoMetaTags lang="en" />
+    <Header />
+    <Container>
+      <HomeHero />
+      <Introduction />
+      <HomeBanner />
+      <Footer />
+    </Container>
+  </Theme>
+);
 
 export default Home;
